5c4f03c4744b54210ae0e803a3bace8657156846
bronzetogold <164731903+dong-b@users.noreply.github.com>
2024-09-22 08:33:43 +0900

    [Silver III] Title: 주유소, Time: 96 ms, Memory: 46236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,22 +1,16 @@
 import sys
 input = sys.stdin.readline
-n= int(input())
-road= list(map(int, input().split()))
-price= list(map(int, input().split()))
-min_idx=0
-tot_sum=0
-while True:
-    i=min_idx
-    if i >= n-1:
-        break
-    if i == price.index(min(price[i:])):
-        tot_sum+=price[i]*sum(road[i:])
-        break
-    for x in range(i,n):
-        if price[x]<price[i]:
-            min_idx=x
-            break
-    
-    tot_sum+=price[i]*sum(road[i:min_idx])
-    
+
+n = int(input())
+road = list(map(int, input().split()))
+price = list(map(int, input().split()))
+
+tot_sum = 0
+current_price = price[0]
+
+for i in range(n - 1):
+    if price[i] < current_price:
+        current_price = price[i]
+    tot_sum += current_price * road[i]
+
 print(tot_sum)
