19a5895fd01c4a4e430035023c53b780bbe5d179
potential <164731903+dong-b@users.noreply.github.com>
2024-09-30 16:51:57 +0900

    [Silver IV] Title: 카드2, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,9 @@
-from collections import deque
-import sys
-input=sys.stdin.readline
 n=int(input())
-lst=deque([n for n in range(n,0,-1)])
-while True:
-    if len(lst) == 1:
-        print(lst.pop())
-        break
-    lst.pop()
-    lst.appendleft(lst.pop())
+num=1
+if n ==1:
+    print(1)
+else:
+    while n> num:
+        num*=2
+    num//=2
+    print(2*(n-num))
