fe7b80a521f763efb2a7a7c673c3bbe120347257
potential <164731903+dong-b@users.noreply.github.com>
2024-10-05 21:20:52 +0900

    [Bronze IV] Title: 특별한 날, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,11 @@
+a=int(input())
+b=int(input())
+if a ==2:
+    if b == 18:
+        print("Special")
+    elif b < 18:
+        print("Before")
+    else: print("After")
+elif a >2:
+    print("After")
+else: print("Before")
