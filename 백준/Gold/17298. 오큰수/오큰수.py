38cf94673785fc92e05323742e5c59a07f5d773e
potential <164731903+dong-b@users.noreply.github.com>
2024-10-14 13:42:47 +0900

    [Gold IV] Title: 오큰수, Time: 988 ms, Memory: 155536 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,19 @@
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = list(map(int, input().split()))
+answer = [-1] * n
+stack = []
+
+for i in range(n):
+    while stack and lst[stack[-1]] < lst[i]:
+        answer[stack.pop()] = lst[i] 
+    stack.append(i)  
+
+print(*answer) 
+
+
+
+
